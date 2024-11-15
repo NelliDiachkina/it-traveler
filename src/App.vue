544059542493e90c513b3180661775290f8817e7
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-11-15 19:54:56 +0200

    FavoritePlaces + FavoritePlace

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup>
-import HomepageView from './views/HomepageView.vue'
-
-const isDesktop = window.innerWidth > 1024
-const isTablet = window.innerWidth <= 1024 && window.innerWidth > 748
+import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 </script>
 
 <template>
-  <template v-if="isDesktop">
-    <HomepageView />
-  </template>
-  <template v-else-if="isTablet">This is tablet</template>
-  <template v-else> This is mobile </template>
+  <!-- <HomepageView /> -->
+  <div class="bg-white h-screen w-[400px]">
+    <FavoritePlaces />
+  </div>
 </template>
