0cafacd24683609a5f25c27dc96f34bb2969cb12
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-11-16 18:53:42 +0200

    add v-model

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -2,7 +2,6 @@
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
 import { ref } from 'vue'
-import IInput from '../IInput/IInput.vue'
 
 const buttonVariant = ref('gradient')
 
@@ -13,11 +12,7 @@ const changeButtonVariant = () => {
 
 <template>
   <div class="px-6">
-    <div class="text-gray mb-4">Додані маркери</div>
-    <div class="py-5">
-      <IInput label="Some label" />
-      <a href="/" class="text-black">CLick me</a>
-    </div>
+    <h2 class="text-gray mb-4">Додані маркери</h2>
     <slot name="label"></slot>
     <slot name="list">
       <FavoritePlace v-for="n in 4" :key="n" />
