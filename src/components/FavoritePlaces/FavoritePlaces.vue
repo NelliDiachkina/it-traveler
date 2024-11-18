69f0ece7f64e2f320e2c7cf7fb04211e1bb0c5e8
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-11-18 21:10:35 +0200

    Add map

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,26 +1,18 @@
 <script setup>
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
-import { ref } from 'vue'
-
-const buttonVariant = ref('gradient')
-
-const changeButtonVariant = () => {
-  buttonVariant.value = buttonVariant.value === 'gradient' ? 'outlined' : 'gradient'
-}
 </script>
 
 <template>
   <div class="px-6">
     <h2 class="text-gray mb-4">Додані маркери</h2>
+
     <slot name="label"></slot>
     <slot name="list">
       <FavoritePlace v-for="n in 4" :key="n" />
     </slot>
 
     <slot></slot>
-    <IButton class="w-full mt-10" :variant="buttonVariant" @click="changeButtonVariant"
-      >Додати маркер</IButton
-    >
+    <IButton class="w-full mt-10" variant="gradient">Додати маркер</IButton>
   </div>
 </template>
