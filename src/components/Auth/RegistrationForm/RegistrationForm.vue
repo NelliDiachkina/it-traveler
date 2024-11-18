dbef93daa7fdc882dfcd5fecf22e514aaca04da6
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-11-18 17:14:23 +0200

    add text-area

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -13,9 +13,13 @@ const userData = reactive({
 </script>
 
 <template>
-  <FormContainer @submit.prevent="emit('submit', toRaw(userData))">
+  <FormContainer
+    @submit.prevent="emit('submit', toRaw(userData))"
+    class="max-w-[500px] w-full bg-white p-10 rounded-2xl"
+  >
     <IInput class="mb-4" label="Повне ім'я" v-model="userData.name" />
     <IInput
+      type="textarea"
       class="mb-4"
       label="Електронна пошта"
       placeholder="email@gmail.com"
