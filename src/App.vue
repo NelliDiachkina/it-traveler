4435dff429abdf8968cbc50ccd9fa9f3d0f1328e
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-11-21 15:15:02 +0200

    Created homepage view + Added rest views

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,11 @@
 <script setup>
-import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
-import { MapboxMap, MapboxMarker } from '@studiometa/vue-mapbox-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
-import { mapSettings } from './map/settings'
-import MarkerIcon from './components/icons/MarkerIcon.vue'
-import { ref } from 'vue'
-
-const favoritePlaces = [
-  {
-    id: 1,
-    title: 'New place 1',
-    description: 'Super description 1',
-    img: '',
-    lngLat: [30.523333, 50.490001]
-  },
-  {
-    id: 2,
-    title: 'New place 2',
-    description: 'Super description 2',
-    img: '',
-    lngLat: [30.5228, 50.420001]
-  },
-  {
-    id: 3,
-    title: 'New place 3',
-    description: 'Super description 3',
-    img: '',
-    lngLat: [30.703683, 50.389739]
-  },
-  {
-    id: 4,
-    title: 'New place 4',
-    description: 'Super description 4',
-    img: '',
-    lngLat: [30.389619, 50.431875]
-  }
-]
-
-const activeId = ref(null)
-const map = ref(null)
-
-const changeActiveId = (id) => {
-  activeId.value = id
-}
-
-const changePlace = (id) => {
-  const { lngLat } = favoritePlaces.find((place) => place.id === id)
-  changeActiveId(id)
-  map.value.flyTo({ center: lngLat })
-}
+// import HomepageView from './views/HomepageView.vue'
+// import GreetingView from './views/GreetingView.vue'
+import AuthView from './views/AuthView.vue'
 </script>
 
 <template>
-  <main class="flex h-screen">
-    <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
-      <FavoritePlaces :items="favoritePlaces" :active-id="activeId" @place-clicked="changePlace" />
-    </div>
-    <div class="w-full h-full flex items-center justify-center text-6xl">
-      <MapboxMap
-        class="w-full h-full"
-        :center="[30.523333, 50.450001]"
-        :zoom="10"
-        :access-token="mapSettings.apiToken"
-        :map-style="mapSettings.style"
-        @mb-created="(mapInstance) => (map = mapInstance)"
-      >
-        <MapboxMarker v-for="place in favoritePlaces" :key="place.id" :lngLat="place.lngLat">
-          <button @click="changeActiveId(place.id)">
-            <MarkerIcon class="h-8 w-8" />
-          </button>
-        </MapboxMarker>
-      </MapboxMap>
-    </div>
-  </main>
+  <!-- <GreetingView /> -->
+  <AuthView />
+  <!-- <HomepageView /> -->
 </template>
