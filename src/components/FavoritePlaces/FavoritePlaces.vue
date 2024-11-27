d946c50682b97f619d224426026836d71427865e
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-11-27 21:54:19 +0200

    Added edit feature for places

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,6 +1,12 @@
 <script setup>
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
+import EditPlaceModal from '../EditPlaceModal/EditPlaceModal.vue'
+import { useModal } from '../../composables/useModal.js'
+import { computed, ref } from 'vue'
+import { useMutation } from '../../composables/useMutation'
+import { updateFavoritePlace } from '../../api/favorite-places'
+// import ConfirmationModal from '../ConfirmationModal/ConfirmationModal.vue'
 
 const props = defineProps({
   items: {
@@ -9,11 +15,31 @@ const props = defineProps({
   },
   activeId: {
     required: true,
-    type: [Number, null]
+    type: [String, null]
   }
 })
 
-const emit = defineEmits(['place-clicked', 'create'])
+const emit = defineEmits(['place-clicked', 'create', 'updated'])
+
+const { isOpen: isEditOpen, openModal: openEditModal, closeModal: closeEditModal } = useModal()
+const { mutation: updatePlace, isLoading } = useMutation({
+  mutationFn: (formData) => updateFavoritePlace(formData),
+  onSuccess: () => {
+    closeEditModal()
+    emit('updated')
+  }
+})
+const selectedId = ref(null)
+const selectedItem = computed(() => props.items.find((place) => place.id === selectedId.value))
+
+const handleEditPlace = (id) => {
+  selectedId.value = id
+  openEditModal()
+}
+
+const handleSubmit = (formData) => {
+  updatePlace(formData)
+}
 </script>
 
 <template>
@@ -31,10 +57,23 @@ const emit = defineEmits(['place-clicked', 'create'])
         :img="place.img"
         :is-active="place.id === props.activeId"
         @click="emit('place-clicked', place.id)"
+        @edit="handleEditPlace(place.id)"
       />
+
+      <EditPlaceModal
+        :is-open="isEditOpen"
+        :place="selectedItem"
+        :is-loading="isLoading"
+        @close="closeEditModal"
+        @submit="handleSubmit"
+      />
+
+      <!-- <ConfirmationModal :is-open="true" title="Ви дійсно хочете видалити улюблене місце?" /> -->
     </slot>
 
     <slot></slot>
-    <IButton class="w-full mt-10" variant="gradient" @click="emit('create')">Додати маркер</IButton>
+    <IButton class="w-full mt-10" variant="gradient" @click="emit('create')">
+      Додати маркер
+    </IButton>
   </div>
 </template>
